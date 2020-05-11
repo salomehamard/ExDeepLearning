2a10c24770aede99ebdb2ac5cfc987f8f63c9883
DDH <dclair@etu.isara.fr>
2020-05-11 17:05:17 +0200

    Correction de l'erreur de code

--- shinytest/Shinytest/ui.R
+++ shinytest/Shinytest/ui.R
@@ -3,11 +3,12 @@ library(shiny)
 ### Partie User Interface
 
 ui <- fluidPage (
+  titlePanel(
+    textOutput("L'application bizarre de Anne-Laure GIRARD,
+               Salomé HAMARD et Dylan CLAIR")
+  ),
   sidebarLayout(
-    titlePanel(
-      textOutput("L'application bizarre de Anne Laure GIRARD,
-                 Salomé HAMARD et Dylan CLAIR")
-    ),
+    
     sidebarPanel(
       textInput("name", "Entrez votre nom ici :")
     ),
