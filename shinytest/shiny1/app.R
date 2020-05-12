41917802096ff3890a1a5daa2b949f737a7e39a3
salomehamard <shamard@etu.isara.fr>
2020-05-12 15:55:41 +0200

    images

--- shinytest/shiny1/app.R
+++ shinytest/shiny1/app.R
@@ -30,8 +30,19 @@ ui <- fluidPage(
                           "Sloubi 2" = "rakatak-mik", "Sloubi 3" = "kayak-kata",
                           "Sloubi 4" = "joue ganoue", "Sloubi 5" = "jeu boulin",
                           "Sloubi 6" = "jeu carré", "Sloubi 7" = "doublette")),
-  textOutput("txtsloubi")  
-)
+  textOutput("txtsloubi"),  
+  titlePanel("Image output"),
+  
+  fluidRow(
+    column(4, wellPanel(
+      radioButtons("picture", "Picture:",
+                   c("Un peu de latin", "C'est pas faut"))
+    )),
+    column(4,
+           imageOutput("image2")
+    )
+  )
+  )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -44,6 +55,26 @@ server <- function(input, output) {
   output$txtsloubi <- renderText({
     paste (input$idsloubi)
 })
+  output$image2 <- renderImage({
+    if (is.null(input$picture))
+      return(NULL)
+    
+    if (input$picture == "C'est pas faut") {
+      return(list(
+        src = "0c6c319447cea2759446e6f2266a69af.png",
+        contentType = "image/png",
+        alt = "C'est pas faut"
+      ))
+    } else if (input$picture == "Un peu de latin") {
+      return(list(
+        src = "missa.png",
+        filetype = "image/png",
+        alt = "Un peu de latin"
+      ))
+    }
+    
+  }, deleteFile = FALSE)
+  
 }
 
 shinyApp(ui=ui, server=server)
